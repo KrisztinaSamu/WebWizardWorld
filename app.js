fca572dd60de5b8273524d3bb0e90861519ae456
Kriszti <letina84@gmail.com>
2023-02-28 17:41:29 +0100

    Build local data container

--- app.js
+++ app.js
@@ -24,5 +24,27 @@ let year = today.getFullYear();
 let fullDate = year + ". " + month + ". " + day + ".";
 document.getElementById("date-today").innerHTML = (fullDate);
 
+// current weather
 
+//http://api.weatherapi.com/v1/current.json?key=ff18cdecca3b4e9cb4e221043210812&q=Budapest&api=no
+
+let target = 'Budapest';
+
+const fetchResults = async(targetLocation) => {
+    let url = `http://api.weatherapi.com/v1/current.json?key=ff18cdecca3b4e9cb4e221043210812&q=${targetLocation}&api=no`;
+    
+    const res = await fetch(url);
+
+    const data = await res.json();
+    
+    console.log(data);
+
+    let locationName = data.location.name;
+    let time = data.location.localtime;
+    let temp = data.current.temp_c;
+    let condition = data.current.condition.text;
+    
+}
+
+fetchResults(target);
 
