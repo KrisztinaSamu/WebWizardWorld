91dd3515590f31bf62fbed7ba97834bf28ccb198
Kriszti <letina84@gmail.com>
2023-03-12 20:18:05 +0100

    Build in focus design to input field

--- app.js
+++ app.js
@@ -1,53 +1,24 @@
-const menu = document.querySelector('.menu');
-const openMenu = document.querySelector('.openMenu');
-const closeMenu = document.querySelector('.closeMenu');
+const menu = document.querySelector(".menu");
+const openMenu = document.querySelector(".openMenu");
+const closeMenu = document.querySelector(".closeMenu");
 
-openMenu.addEventListener('click', show);
-closeMenu.addEventListener('click', close);
+openMenu.addEventListener("click", show);
+closeMenu.addEventListener("click", close);
 
 function show() {
-    menu.style.display='flex';
-    menu.style.top='0';
-    menu.style.right='0';
+  menu.style.display = "flex";
+  menu.style.top = "0";
+  menu.style.right = "0";
 }
 
 function close() {
-    menu.style.top='-100%';
+  menu.style.top = "-100%";
 }
 
 // current date
-
 let today = new Date();
-let month = `${(today.getMonth() + 1) < 10 ? "0" : ""}${today.getMonth() + 1}`;
+let month = `${today.getMonth() + 1 < 10 ? "0" : ""}${today.getMonth() + 1}`;
 let day = `${today.getDate() < 10 ? "0" : ""}${today.getDate()}`;
 let year = today.getFullYear();
 let fullDate = year + ". " + month + ". " + day + ".";
-document.getElementById("date-today").innerHTML = (fullDate);
-
-/* under construction
-
-// current weather
-
-//http://api.weatherapi.com/v1/current.json?key=...
-
-let target = 'Budapest';
-
-const fetchResults = async(targetLocation) => {
-    let url = `http://api.weatherapi.com/v1/current.json?key=...=${targetLocation}&api=no`;
-    
-    const res = await fetch(url);
-
-    const data = await res.json();
-    
-    console.log(data);
-
-    let locationName = data.location.name;
-    let time = data.location.localtime;
-    let temp = data.current.temp_c;
-    let condition = data.current.condition.text;
-
-}
-
-fetchResults(target);
-
-*/
+document.getElementById("date-today").innerHTML = fullDate;
